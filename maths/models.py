50ba44a541403258829fad46ac69c73569655d63
jonas <jonas.refsnes@gmail.com>
2017-01-30 14:31:55 +0100

    Test 1

--- maths/models.py
+++ maths/models.py
@@ -57,34 +57,3 @@ class GeogebraAnswer(models.Model):
 
 # ------------------------------------------------------------------------------------------------------------------------------------
 
-
-class School(models.Model):
-    school_name = models.CharField(max_length=100)
-    school_address = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.school_name
-
-
-class Grade(models.Model):
-    school = models.ForeignKey(School)
-    grade_name = models.CharField(max_length=100)
-    tests = models.ManyToManyField(Test)
-
-    def __str__(self):
-        return self.school.school_name + " - " + self.grade_name
-
-
-class Student(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    grade = models.ForeignKey(Grade, default="", blank=True, null=True)
-    SEX = [
-        ("M", "Gutt"),
-        ("F", "Jente")
-    ]
-    sex = models.CharField(max_length=1, choices=SEX)
-
-    def __str__(self):
-        return self.user.username
-
-
