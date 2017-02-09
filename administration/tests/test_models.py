3cb7f3c00ff8f09a0fe8351981fc023acbe6db7a
haldor <haldormyre@gmail.com>
2017-02-09 12:17:06 +0100

    testPerson

--- administration/tests/test_models.py
+++ administration/tests/test_models.py
@@ -1,7 +1,7 @@
 import pytest
 from mixer.backend.django import mixer
 pytestmark = pytest.mark.django_db
-
+import ipdb;
 
 class TestSchool:
     def test_init(self):
@@ -17,3 +17,17 @@ class TestGrade:
     def test_init(self):
         obj = mixer.blend('administration.Grade')
         assert obj.pk == 1, 'Should save an instance'
+
+class TestPerson:
+    def test_init(self):
+        schoolObj = mixer.blend('administration.School', school_name = 'teSkole')
+        gradeObj = mixer.blend('administration.Grade', school = schoolObj)
+        obj = mixer.blend('administration.Person', grade = gradeObj)
+        assert obj.pk == 1, 'should save an instance'
+
+    def test_createUsername(self):
+        obj = mixer.blend('administration.Person',first_name='Test',last_name='Testesen')
+        assert obj.createusername() == 'testt', 'Should give obj the username "testt"'
+        obj2 = mixer.blend('administration.Person',first_name='Test',last_name='Testesen')
+        obj2.createusername()
+        ipdb.set_trace()
