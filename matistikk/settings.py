63103a43ed6b901c61c7cdc6a3ea9bb90d4fa56d
Sondre Oldervoll <son_oldervoll@hotmail.com>
2017-03-15 11:49:14 +0100

    Lagt til "Lag Oppgave"

--- matistikk/settings.py
+++ matistikk/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_excel',
     'maths',
     'administration',
+    'ckeditor',
 
 
 ]
@@ -136,6 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
 
 AUTH_USER_MODEL = 'administration.Person'
 
