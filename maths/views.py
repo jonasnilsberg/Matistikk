ed6dbfc8006446789d7d89fbfd60864fd5823af2
Sondre Oldervoll <son_oldervoll@hotmail.com>
2017-04-12 16:18:28 +0200

    Endringer på personCreateView

--- maths/views.py
+++ maths/views.py
@@ -2,7 +2,7 @@ from django.views import generic
 from braces.views import LoginRequiredMixin
 from django.core.urlresolvers import reverse_lazy, reverse
 from .forms import CreateTaskForm, CreateCategoryForm, CreateTestForm
-from .models import Task, MultipleChoiceTask, Category, GeogebraTask, Test, TaskOrder, TaskCollection
+from .models import Task, MultipleChoiceTask, Category, GeogebraTask, Test, TaskOrder, TaskCollection, Answer
 from braces import views
 from django.http import JsonResponse
 from administration.models import Grade, Person, Gruppe, School
@@ -434,3 +434,20 @@ class TestCreateView(generic.CreateView):
                 x += 1
         return super(TestCreateView, self).form_valid(form)
 
+
+class AnswerCreateView(generic.CreateView):
+    model = Answer
+    template_name = 'maths/answer_form.html'
+    pk_url_kwarg = 'test_pk'
+    success_url = '/'
+    fields = ['task', 'test', 'user']
+
+    def get_context_data(self, **kwargs):
+        context = super(AnswerCreateView, self).get_context_data(**kwargs)
+        test = Test.objects.get(id=self.kwargs.get('test_pk'))
+        geogebratasks = GeogebraTask.objects.filter(task__in=test.task_collection.tasks.all())
+        options = MultipleChoiceTask.objects.filter(task__in=test.task_collection.tasks.all())
+        context['test'] = test
+        context['geogebratask'] = geogebratasks
+        context['options'] = options
+        return context
