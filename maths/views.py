a973f6d4a578fe0b250f34e85c96a6bdba7fb0ed
Sondre Oldervoll <son_oldervoll@hotmail.com>
2017-04-19 10:43:50 +0200

    Answer form

--- maths/views.py
+++ maths/views.py
@@ -1,14 +1,12 @@
 from django.views import generic
 from braces.views import LoginRequiredMixin
 from django.core.urlresolvers import reverse_lazy, reverse
-from .forms import CreateTaskForm, CreateCategoryForm, CreateTestForm
+from .forms import CreateTaskForm, CreateCategoryForm, CreateTestForm, CreateAnswerForm
 from .models import Task, MultipleChoiceTask, Category, GeogebraTask, Test, TaskOrder, TaskCollection, Answer
 from braces import views
 from django.http import JsonResponse
 from administration.models import Grade, Person, Gruppe, School
-import datetime
-
-
+import random
 
 
 class IndexView(LoginRequiredMixin, generic.TemplateView):
@@ -504,19 +502,22 @@ class TestDetailView(views.AjaxResponseMixin, generic.DetailView):
         context['groups'] = Gruppe.objects.filter(tests__exact=test)
         return context
 
+
 class AnswerCreateView(generic.CreateView):
     model = Answer
+    fields = ['task', 'test', 'user']
     template_name = 'maths/answer_form.html'
     pk_url_kwarg = 'test_pk'
     success_url = '/'
-    fields = ['task', 'test', 'user']
 
     def get_context_data(self, **kwargs):
         context = super(AnswerCreateView, self).get_context_data(**kwargs)
         test = Test.objects.get(id=self.kwargs.get('test_pk'))
         geogebratasks = GeogebraTask.objects.filter(task__in=test.task_collection.tasks.all())
         options = MultipleChoiceTask.objects.filter(task__in=test.task_collection.tasks.all())
+        randomtest = sorted(test.task_collection.tasks.all(), key=lambda x: random.random())
         context['test'] = test
+        context['randomtest'] = randomtest
         context['geogebratask'] = geogebratasks
         context['options'] = options
         return context
