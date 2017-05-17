f7dd681d1af8a9fe372272c219f28aaaf7034a17
haldor <haldormyre@gmail.com>
2017-05-17 09:48:51 +0200

    µðµªðµπªµðµªµðªµŋnœπ→nªðđ

---
+++ maths/tests/test_create_task.py
@@ -0,0 +1,54 @@
+from django.test import LiveServerTestCase
+from administration.models import Person
+from maths.models import Task
+from mixer.backend.django import mixer
+from selenium import webdriver
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class CreateTaskTestCase(LiveServerTestCase):
+    def setUp(self):
+        obj = mixer.blend('administration.Person', role=4, username='admin')
+        obj.set_password('admin')  # Password has to be set like this because of the hash-function
+        obj.save()
+
+
+        #  Webdriver setup
+        self.selenium = webdriver.Chrome()
+        self.selenium.maximize_window()
+        super(CreateTaskTestCase, self).setUp()
+
+    def tearDown(self):
+        self.selenium.quit()
+        super(CreateTaskTestCase, self).tearDown()
+
+    # Confirms scenario 2.16
+    def test_admin_can_create_task(self):
+        self.selenium.get(
+            '%s%s' % (self.live_server_url, "/")
+        )
+        WebDriverWait(self.selenium, 10).until(EC.presence_of_element_located((By.ID, "id_username")))
+        # Fill login information of admin
+        username = self.selenium.find_element_by_id('id_username')
+        username.send_keys("admin")
+        password = self.selenium.find_element_by_id('id_password')
+        password.send_keys("admin")
+        self.selenium.find_element_by_id('logInBtn').click()
+        WebDriverWait(self.selenium, 10).until(EC.presence_of_element_located((By.ID, "createTask")))
+        self.selenium.find_element_by_id('createTask').click()
+        self.selenium.find_element_by_id('taskname').send_keys('testoppgave')
+        self.selenium.find_element_by_id('addCategoryBtn').click()
+        time.sleep(0.5)  # wait for modal open
+        self.selenium.find_element_by_id('id_category_title').send_keys('testKategori')
+        self.selenium.find_element_by_id('saveCategoryBtn').click()
+        time.sleep(0.5)  # wait for modal close
+        self.selenium.find_element_by_id('tasktext_ifr').send_keys('Dette er oppgaveteksten')
+        self.selenium.find_element_by_id('textAnswerRadio').click()
+        self.selenium.find_element_by_id('extraField').click()
+        self.selenium.find_element_by_id('saveTaskBtn').click()
+        time.sleep(0.2)
+        self.assertEqual(1, len(Task.objects.all()))
---
+++ maths/tests/test_taskoverview.py
@@ -0,0 +1,56 @@
+from django.test import LiveServerTestCase
+from administration.models import Person
+from maths.models import Task
+from mixer.backend.django import mixer
+from selenium import webdriver
+import time
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class CreateTaskTestCase(LiveServerTestCase):
+    def setUp(self):
+        obj = mixer.blend('administration.Person', role=4, username='admin')
+        obj.set_password('admin')  # Password has to be set like this because of the hash-function
+        obj.save()
+
+        taskObj = mixer.blend('maths.Task', title='testOppgave')
+        taskObj.save()
+
+        #  Webdriver setup
+        self.selenium = webdriver.Chrome()
+        self.selenium.maximize_window()
+        super(CreateTaskTestCase, self).setUp()
+
+    def tearDown(self):
+        self.selenium.quit()
+        super(CreateTaskTestCase, self).tearDown()
+
+    # Confirms scenario 17
+    def test_admin_can_create_task(self):
+        self.selenium.get(
+            '%s%s' % (self.live_server_url, "/")
+        )
+        WebDriverWait(self.selenium, 10).until(EC.presence_of_element_located((By.ID, "id_username")))
+        # Fill login information of admin
+        username = self.selenium.find_element_by_id('id_username')
+        username.send_keys("admin")
+        password = self.selenium.find_element_by_id('id_password')
+        password.send_keys("admin")
+        self.selenium.find_element_by_id('logInBtn').click()
+        WebDriverWait(self.selenium, 10).until(EC.presence_of_element_located((By.ID, "overviewDropdown")))
+        self.selenium.find_element_by_id('overviewDropdown').click()
+        WebDriverWait(self.selenium, 10).until(EC.presence_of_element_located((By.ID, "taskOverview")))
+        self.selenium.find_element_by_id('taskOverview').click()
+
+        WebDriverWait(self.selenium, 10).until(EC.presence_of_element_located((By.ID, "search")))
+        self.selenium.find_element_by_id('search').send_keys('testOpp')
+        task_list = self.selenium.find_element_by_id('tasktable')
+        eval = False
+        for el in task_list.find_elements_by_tag_name('td'):
+            if el.text == 'testOppgave':
+                eval = True
+                break
+        self.assertTrue(eval)
