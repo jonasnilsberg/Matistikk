8d392c614156085091bf9278b55ea89886f4f9d2
jonas <jonas.refsnes@gmail.com>
2017-06-06 15:29:26 +0200

    Små endringer overalt

--- matistikk/settings.py
+++ matistikk/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'matistikk.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-"""     THIS DATABASE WAS USED IN DEVELOPMENT 
+"""     THIS DATABASE WAS USED IN DEVELOPMENT """
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -94,7 +94,7 @@ DATABASES = {
 
         },
     }
-}"""
+}
 
 ALLOWED_HOSTS = ["192.168.1.113", "127.0.0.1", "localhost", "192.168.1.22", "158.38.120.33"]
 # Password validation
@@ -137,13 +137,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(PROJECT_DIR, 'lokaldb.db'),
     }
-}
+}"""
 
 AUTH_USER_MODEL = 'administration.Person'
 
